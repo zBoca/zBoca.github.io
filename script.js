0913880bab244d5214508aa42e46381b51addc0f
zBoca <jacob99709@gmail.com>
2024-06-21 18:20:26 -0700

    6/21/2024
    New portfolio website

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".mobileLinks")
-    const icon = document.querySelector(".mobileIcon")
-
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
