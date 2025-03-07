abff4ac9a05f8cd5f42363f03e3f6d47e7418e88
zBoca <jacob99709@gmail.com>
2025-03-07 12:49:02 -0800

    Adding 3d Section

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu()
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function toggleProjects()
+{
+    const twod = document.querySelector(".twod")
+    const twodcontainer = document.querySelector(".twod-container")
+    const threed = document.querySelector(".threed")
+    const threedcontainer = document.querySelector(".threed-container")
+
+    twod.classList.toggle("selected")
+    twodcontainer.classList.toggle("selected")
+    threed.classList.toggle("selected")
+    threedcontainer.classList.toggle("selected")
+}
