37f833cde53fd1c3ba8c8da440933ccf1dafb613
zBoca <jacob99709@gmail.com>
2025-06-12 12:58:54 -0700

    fix 6-12-2025
    Fixed formatting and fluidity problems
    
    Updated the projects pages to be more readable

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-function toggleMenu() 
-{
-    const menu = document.querySelector(".menuLinks");
-    const icon = document.querySelector(".hamburgerIcon");
-
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
-
-function toggleProjects()
-{
-    const twod = document.querySelector(".twod")
-    const twodcontainer = document.querySelector(".twod-container")
-    const threed = document.querySelector(".threed")
-    const threedcontainer = document.querySelector(".threed-container")
-
-    twod.classList.toggle("selected")
-    twodcontainer.classList.toggle("selected")
-    threed.classList.toggle("selected")
-    threedcontainer.classList.toggle("selected")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
